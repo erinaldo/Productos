95127b9b4c8a25df925b2f1f40b142d324b15d06
E3 - Systemas <vendas@mastercaixas.com>
2021-05-31 14:05:45 -0300

    Delete Effort directory

--- Effort/trunk/Codigo/Web/Effort/Effort/Models/Effort.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\El.Eduardo_Windows\Documents\Duxstar\Repositorios\Productos\Effort\trunk\Codigo\Web\Effort\Effort\Models\Effort.edmx'. 
-// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
-// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
-// el modelo en el diseñador.
