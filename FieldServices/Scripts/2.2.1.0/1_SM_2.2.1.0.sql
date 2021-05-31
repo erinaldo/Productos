94ff86d80e36e7f64ea2cda66103914c01003f7c
E3 - Systemas <vendas@mastercaixas.com>
2021-05-31 14:08:11 -0300

    Delete FieldServices directory

--- FieldServices/Scripts/2.2.1.0/1_SM_2.2.1.0.sql
+++
@@ -1,3 +0,0 @@
-IF NOT EXISTS (SELECT * FROM Mensaje WHERE ClaveMensaje = 'MP0033')
-	INSERT INTO Mensaje ( ClaveMensaje, Descripcion,Ambiente) VALUES('MP0033','Es necesario enviar los datos capturados en la jornada, antes de solicitar los datos de alguna jornada',3)
-
